26b10e4f497a2b6e3cff4203e3c85c908d9d888e
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2022-12-01 15:21:46 -0500

    LIBWEB-5754. Initial commit for FAQ microservice.
    https://issues.umd.edu/browse/LIBWEB-5754

---
+++ README.md
@@ -0,0 +1,81 @@
+# faq-searcher
+
+Python 3 Flask application to search the libanswers FAQ searcher.
+
+## Requires
+
+* Python 3.10.8
+
+### Development Setup
+
+See [docs/DevelopmentSetup.md](docs/DevelopmentSetup.md).
+
+### Running in Docker
+
+```bash
+$ docker build -t docker.lib.umd.edu/faq-searcher .
+$ docker run -it --rm -p 5000:5000 --env-file=.env --read-only docker.lib.umd.edu/faq-searcher
+```
+
+### Building for Kubernetes
+
+```bash
+$ docker buildx build . --builder=kube -t docker.lib.umd.edu/faq-searcher:VERSION --push
+```
+
+### Endpoints
+
+This will start the webapp listening on the default port 5000 on localhost
+(127.0.0.1), and running in [Flask's debug mode].
+
+Root endpoint (just returns `{status: ok}` to all requests):
+<http://localhost:5000/>
+
+/ping endpoint (just returns `{status: ok}` to all requests):
+<http://localhost:5000/ping>
+
+/search:
+
+```bash
+http://localhost:5000/search?q=print&per_page=3&page=0
+```
+
+Example:
+
+```bash
+curl 'http://localhost:5000/search?q=print&per_page=3&page=0'
+{
+  "endpoint": "faq",
+  "module_link": "http://umd.libanswers.com/search?q=print",
+  "page": 0,
+  "per_page": 3,
+  "query": "print",
+  "results": [
+    {
+      "description": "Photocopying, Printing, Library 101",
+      "item_format": "web_page",
+      "link": "https://umd.libanswers.com/faq/66289",
+      "title": "How do I print?"
+    },
+    {
+      "description": "Printing, Makerspace",
+      "item_format": "web_page",
+      "link": "https://umd.libanswers.com/faq/66460",
+      "title": "Can I 3D Print in two different colors?"
+    },
+    {
+      "description": "Photocopying, Printing, Library 101",
+      "item_format": "web_page",
+      "link": "https://umd.libanswers.com/faq/66032",
+      "title": "How can I print and/or make copies?"
+    }
+  ],
+  "total": 42
+}
+```
+
+[Flask's debug mode]: https://flask.palletsprojects.com/en/2.2.x/cli/?highlight=debug%20mode
+
+## License
+
+See the [LICENSE](LICENSE.txt) file for license rights and limitations.
---
+++ src/app.py
@@ -0,0 +1,180 @@
+import json
+import logging
+import furl
+import os
+import requests
+
+from flask import Flask, request
+from dotenv import load_dotenv
+from waitress import serve
+from paste.translogger import TransLogger
+
+load_dotenv('../.env')
+
+env = {}
+for key in ('LIBANSWERS_API_BASE', 'IID', 'NO_RESULTS_URL', 'MODULE_URL'):
+    env[key] = os.environ.get(key)
+    if env[key] is None:
+        raise RuntimeError(f'Missing environment variable: {key}')
+
+siteid = env['IID']
+no_results_url = env['NO_RESULTS_URL']
+module_url = env['MODULE_URL']
+
+debug = os.environ.get('FLASK_DEBUG')
+
+logger = logging.getLogger('faq-searcher')
+loggerWaitress = logging.getLogger('waitress')
+
+if debug:
+    logger.setLevel(logging.DEBUG)
+    loggerWaitress.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+    loggerWaitress.setLevel(logging.INFO)
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/')
+def root():
+    return {'status': 'ok'}
+
+
+@app.route('/ping')
+def ping():
+    return {'status': 'ok'}
+
+
+@app.route('/search')
+def search():
+    args = request.args
+
+    # Check query param
+    if 'q' not in args or args['q'] == '':
+        return {
+            'error': {
+                'msg': 'q parameter is required',
+            },
+        }, 400
+    query = args['q']
+
+    limit = 3
+    if 'per_page' in args and args['per_page'] != "":
+        limit = int(args['per_page'])
+
+    offset = 0
+    page = 1
+    if 'page' in args and args['page'] != "":
+        page = int(args['page'])
+        if page > 1:
+            offset = limit * (page - 1) + 1
+
+    logger.debug(f'Pagination debug offset={offset} page={page} limit={limit}')
+
+    # Prepare OCLC API search
+    params = {
+        'iid': siteid,
+        'page': page,
+        'limit': limit,
+    }
+
+    full_query_url = env['LIBANSWERS_API_BASE'] + query
+
+    search_url = furl.furl(full_query_url)
+
+    # Execute Libanswers API search
+    try:
+        response = requests.get(search_url.url, params=params)
+    except Exception as err:
+        logger.error(f'Search error at url'
+                     '{search_url.url}, params={params}\n{err}')
+
+        return {
+            'endpoint': 'faq',
+            'error': {
+                'msg': f'Search error',
+            },
+        }, 500
+
+    if response.status_code not in [200, 206]:
+        logger.error(f'Received {response.status_code} with q={query}')
+
+        return {
+            'endpoint': 'faq',
+            'error': {
+                'msg': f'Received {response.status_code} for q={query}',
+            },
+        }, 500
+
+    logger.debug(f'Submitted url={search_url.url}, params={params}')
+    logger.debug(f'Received response {response.status_code}')
+
+    json_content = json.loads(response.text)
+    total_records = get_total_records(json_content)
+
+    module_link = module_url + query
+
+    api_response = {
+        'endpoint': 'faq',
+        'query': query,
+        'per_page': limit,
+        'page': page,
+        'total': total_records,
+        'module_link': module_link,
+    }
+
+    if debug:
+        api_response['raw'] = json_content
+
+    if total_records != 0:
+        api_response['results'] = build_response(json_content)
+    else:
+        api_response['error'] = build_no_results()
+
+    return api_response
+
+
+def build_no_results():
+    return {
+        'msg': 'No Results',
+        'no_results_url': no_results_url,
+    }
+
+
+def build_response(json_content):
+    results = []
+    if 'search' in json_content:
+        search = json_content['search']
+        if 'results' in search:
+            for item in search['results']:
+                topics = None
+                if 'topics' in item:
+                    topics = ", ".join(item['topics'])
+                results.append({
+                    'title': item['question'],
+                    'item_format': 'web_page',
+                    'link': item['url'],
+                    'description': topics,
+                })
+    return results
+
+
+def get_total_records(json_content):
+    if 'search' not in json_content:
+        return None
+    search = json_content['search']
+    if 'numFound' not in search:
+        return None
+    return int(search['numFound'])
+
+
+if __name__ == '__main__':
+    # This code is not reached when running "flask run". However the Docker
+    # container runs "python app.py" and host='0.0.0.0' is set to ensure
+    # that flask listens on port 5000 on all interfaces.
+
+    # Run waitress WSGI server
+    serve(TransLogger(app, setup_console_handler=True),
+          host='0.0.0.0', port=5000, threads=10)
